a447ac396e10cb6b0a50b318606d89ba57355f72
Joshua David Gardner <jgardn11@yahoo.com>
2023-02-11 15:20:44 +0000

    Complete

--- src/main/java/Average.java
+++ src/main/java/Average.java
@@ -18,6 +18,13 @@ public class Average {
      * @return the average of all the values of nums.
      */
     public double getAvg(int[] nums){
-        return 0;
+        int numsTotal = 0;
+        for (int i = 0; i < nums.length; i++){
+            numsTotal += nums[i];
+        }
+
+        double numsAvg = 0;
+        numsAvg = numsTotal/ (double) nums.length;
+        return numsAvg;
     }
 }
